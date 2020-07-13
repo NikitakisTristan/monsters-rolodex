47ad4043d05d53ead3dced1ee667dbdaec424157
NikitakisTristan <tristan.nikitakis@gmail.com>
2020-07-13 14:56:26 -0400

    Updates

---
+++ precache-manifest.e7f486dbfdcbfe185a8ad5af04a62387.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "561c4bb0ed28f46fac399fc1063d6bbd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b68bf31a14d01df5c7e3",
+    "url": "/monsters-rolodex/static/css/main.63a93d04.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b68bf31a14d01df5c7e3",
+    "url": "/monsters-rolodex/static/js/main.dc58d5da.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
